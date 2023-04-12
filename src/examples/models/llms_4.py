af86379b822f516877a6791bdfd27fcec0a5f2a9
a <a@a.com>
2023-04-12 16:39:34 -0400

    intro to some of the properties and methods of a Base LLM class

---
+++ src/examples/models/llms_4.py
@@ -0,0 +1,25 @@
+from langchain.llms.base import LLM
+from typing import Optional, List, Mapping, Any
+
+class CustomLLM(LLM):
+    
+    n: int
+        
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+    
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        if stop is not None:
+            raise ValueError("stop kwargs are not permitted.")
+        return prompt[:self.n]
+    
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {"n": self.n}
+    
+def main():
+    llm = CustomLLM(n=10)
+    llm("This is a foobar thing")
+    print(llm)
