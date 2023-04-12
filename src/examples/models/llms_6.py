56ffd67bb73b1a9c347170f3b72aec1fbfaf9c11
a <a@a.com>
2023-04-12 18:01:31 -0400

    in memory cache example

---
+++ src/examples/models/llms_6.py
@@ -0,0 +1,20 @@
+import time
+from langchain.llms import OpenAI
+import langchain
+from langchain.cache import InMemoryCache
+
+def main():
+    langchain.llm_cache = InMemoryCache()
+    # To make the caching really obvious, lets use a slower model.
+    llm = OpenAI(model_name="text-davinci-002", n=2, best_of=2)
+    tic = time.perf_counter()
+    # The first time, it is not yet in cache, so it should take longer
+    llm("Tell me a joke")
+    toc = time.perf_counter()
+    print(f"{toc - tic:0.4f} seconds")
+
+    tic = time.perf_counter()
+    # The first time, it is not yet in cache, so it should take longer
+    llm("Tell me a joke")
+    toc = time.perf_counter()
+    print(f"{toc - tic:0.4f} seconds")
