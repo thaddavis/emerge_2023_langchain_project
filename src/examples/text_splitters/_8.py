376ead098a8468fc685c437f2a1fbe3512e0aca3
a <a@a.com>
2023-04-13 17:49:46 -0400

    RecursiveCharacterTextSplitter

---
+++ src/examples/text_splitters/_8.py
@@ -0,0 +1,21 @@
+# TLDR
+"""RecursiveCharacterTextSplitter"""
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# This is a long document we can split up.
+with open('./src/examples/text_splitters/state_of_the_union.txt') as f:
+    state_of_the_union = f.read()
+
+def main():
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 100,
+        chunk_overlap  = 20,
+        length_function = len,
+    )
+
+    texts = text_splitter.create_documents([state_of_the_union])
+    print(texts[0])
+    print(texts[1])
